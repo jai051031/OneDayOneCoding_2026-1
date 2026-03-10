f74e8cb79126ee2b42c9486bce5239bba98adccf
jai051031 <161295087+jai051031@users.noreply.github.com>
2026-03-10 22:29:01 +0900

    Update 블랙잭.cpp

--- Day1~Day10/Day1/블랙잭.cpp
+++ Day1~Day10/Day1/블랙잭.cpp
@@ -1,1 +1,23 @@
-
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+    int N, M, A[100], MAX = 0;
+    cin >> N >> M;
+    for(int i=0; i<N; i++){
+        cin >> A[i];
+    }
+    for(int i=0; i<N; i++){
+        for(int j=1; j<N; j++){
+            for(int k=2; k<N; k++){
+                if(i != j && j != k && k != i){
+                    if(A[i] + A[j] + A[k] <= M){
+                        if(A[i] + A[j] + A[k] > MAX)
+                            MAX = A[i] + A[j] + A[k];
+                    }
+                }
+            }
+        }
+    }
+    cout << MAX;
+}
